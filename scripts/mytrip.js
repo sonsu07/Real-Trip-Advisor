d8ec03212fe58586e947f0b011e28d86a450584a
sonsu <sonsuwdev@gmail.com>
2022-10-04 16:46:26 +0900

    저장한 장소 제거 기능 완료

--- scripts/mytrip.js
+++ scripts/mytrip.js
@@ -1,8 +1,9 @@
 import {getCookie, setCookie, deleteCookie} from "../module/cookie.js";
 
 let map;
+let MARKER_LABELS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-function initMap() {
+function initMap(lat, lng) {
     let mapTag = document.querySelector('#map');
     map = new google.maps.Map(mapTag, {
         zoom: 12,
@@ -22,21 +23,48 @@ function getData() {
         myTrips = JSON.parse(myTrips);
     }
 
-    initMap();
+    initMap(myTrips.x, myTrips.y);
 
     // 쿠키 데이터 잡아와서 마리 리스트 생성
     let myTripList = document.querySelector('#mytrip-list');
-    myTrips.forEach(data => {
-        let item = generateMyTripList(data);
+    let bounds = new google.maps.LatLngBounds();
+    myTrips.forEach((data, idx) => {
+        let markerLabel = MARKER_LABELS[idx];
+
+        let item = generateMyTripList(data, markerLabel);
         myTripList.append(item);
+
+        let pos = {
+            lat: data.x,
+            lng: data.y
+        };
+
+        // Map에 Marker 생성
+        new google.maps.Marker({
+            position: pos,
+            laebl: markerLabel,
+            map: map
+        });
+        // 위도, 경도로 구성된 좌표 오브젝트를 삽입.
+        // 해당 영역이 좌표를 모두 포함할 수 있을정도로 넓어짐.
+        bounds.extend(pos);
     })
+    // 표현해야 할 좌표를 모두 보여주는 최적의 zoom 레벨을 자동으로 생성
+    map.fitBounds(bounds);
 }
 
-function generateMyTripList(data) {
+function generateMyTripList(data, markerLabel) {
     const template = document.querySelector('#mytrip-item-template').cloneNode(true);
     template.removeAttribute('id');
-    template.querySelector('.item-name').innerHTML = data.name;
+    template.querySelector('.item-name').innerHTML = `${markerLabel}. ${data.name}`;
     template.querySelector('.item-city-name').innerHTML = data.cityName;
+
+    template.querySelector('.item-remove').addEventListener('click', function (e) {
+        e.preventDefault();
+
+        console.log(this);
+        this.closest('.mytrip-item').remove(); // 겉보기에만 제거
+    })
 
     return template
 }
